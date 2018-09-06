cae69dbbd0c38f2978a1407e86f3730dd6b4ded2
Anton Bialenik <tony.baton@yandex.by>
2018-09-06 12:27:56 +0300

    Started project, done for data source in gridview

---
+++ OneToManyWFApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Windows.Forms;
+
+namespace OneToManyWFApp
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+
+        public int? TeamId { get; set; }
+        public virtual Team Team { get; set; }
+    }
+
+    public class Team
+    {
+        public Team()
+        {
+            Players = new List<Player>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Coach { get; set; }
+
+        public virtual ICollection<Player> Players { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    internal class SoccerContext : DbContext
+    {
+        public SoccerContext() : base("SoccerDb")
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
